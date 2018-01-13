f806c338f1b3572a7604d1b2d3d18b52852123b6
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2018-01-12 21:52:50 -0600

    Updated header files

---
+++ macros/src/Harmonics.h
@@ -0,0 +1,7 @@
+#include "Harmonics/setYrange.C"
+#include "Harmonics/N1.C"
+#include "Harmonics/N1EVEN.C"
+#include "Harmonics/N112.C"
+/* #include "Harmonics/N123.C" */
+#include "Harmonics/N2.C"
+#include "Harmonics/N3.C"
