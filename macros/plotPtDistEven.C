88c44f251c5a073bc8a5a2f11e6e5400ddd80cad
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2018-01-21 13:37:02 -0600

    Update

--- macros/plotPtDistEven.C
+++ macros/plotPtDistEven.C
@@ -22,6 +22,10 @@ static const int cmaxCENT[] = {5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 20, 60
 static const int netabins = 12;
 static const double etabins[] = {-2.4, -2.0, -1.6, -1.2, -0.8, -0.4,  0.0,  0.4,  0.8,  1.2,  1.6,  2.0,  2.4};
 static const double etaMid[] = {-2.2, -1.8, -1.4, -1.0, -0.6, -0.2,  0.2,  0.6,  1.0,  1.4,  1.8,  2.2};
+TString etags[] = {"-2.4", "-2.0", "-1.6", "-1.2", "-0.8", "-0.4", "00.0", "00.4", "00.8", "01.2", "01.6", "02.0", "02.4"};
+static const int nptbins = 18;
+static const double ptbins[] = {0.30,  0.40,  0.50,  0.60,  0.80,  1.00,  1.25,  1.50,  2.00,  2.50,  3.00,
+                     3.50,  4.00,  5.00,  6.00,  7.00,  8.00,  10.00,  12.00};
 static int NANALS = 24;
 TString ANAL[] = {"N1MCm22SUB2", "N1MCm18SUB2", "N1MCm14SUB2", "N1MCm10SUB2", "N1MCm06SUB2", "N1MCm02SUB2",
                  "N1MCp02SUB2", "N1MCp06SUB2", "N1MCp10SUB2", "N1MCp14SUB2", "N1MCp18SUB2", "N1MCp22SUB2",
@@ -29,95 +33,61 @@ TString ANAL[] = {"N1MCm22SUB2", "N1MCm18SUB2", "N1MCm14SUB2", "N1MCm10SUB2", "N
                  "N1MCp02SUB3", "N1MCp06SUB3", "N1MCp10SUB3", "N1MCp14SUB3", "N1MCp18SUB3", "N1MCp22SUB3"
 };
 
-TH1D * gN1MCm22SUB2_pt[ncentbins];
-TH1D * gN1MCm18SUB2_pt[ncentbins];
-TH1D * gN1MCm14SUB2_pt[ncentbins];
-TH1D * gN1MCm10SUB2_pt[ncentbins];
-TH1D * gN1MCm06SUB2_pt[ncentbins];
-TH1D * gN1MCm02SUB2_pt[ncentbins];
+TH1D * gN1MCm22SUB3_pt[netabins][ncentbins];
+TH1D * gN1MCm18SUB3_pt[netabins][ncentbins];
+TH1D * gN1MCm14SUB3_pt[netabins][ncentbins];
 
-TH1D * gN1MCp22SUB2_pt[ncentbins];
-TH1D * gN1MCp18SUB2_pt[ncentbins];
-TH1D * gN1MCp14SUB2_pt[ncentbins];
-TH1D * gN1MCp10SUB2_pt[ncentbins];
-TH1D * gN1MCp06SUB2_pt[ncentbins];
-TH1D * gN1MCp02SUB2_pt[ncentbins];
+TH1D * gN1MCp22SUB3_pt[netabins][ncentbins];
+TH1D * gN1MCp18SUB3_pt[netabins][ncentbins];
+TH1D * gN1MCp14SUB3_pt[netabins][ncentbins];
 
-TH1D * gN1MC22SUB2_pt[ncentbins];
-TH1D * gN1MC18SUB2_pt[ncentbins];
-TH1D * gN1MC14SUB2_pt[ncentbins];
-TH1D * gN1MC10SUB2_pt[ncentbins];
-TH1D * gN1MC06SUB2_pt[ncentbins];
-TH1D * gN1MC02SUB2_pt[ncentbins];
-
-TH1D * gN1MCm22SUB3_pt[ncentbins];
-TH1D * gN1MCm18SUB3_pt[ncentbins];
-TH1D * gN1MCm14SUB3_pt[ncentbins];
-TH1D * gN1MCm10SUB3_pt[ncentbins];
-TH1D * gN1MCm06SUB3_pt[ncentbins];
-TH1D * gN1MCm02SUB3_pt[ncentbins];
-
-TH1D * gN1MCp22SUB3_pt[ncentbins];
-TH1D * gN1MCp18SUB3_pt[ncentbins];
-TH1D * gN1MCp14SUB3_pt[ncentbins];
-TH1D * gN1MCp10SUB3_pt[ncentbins];
-TH1D * gN1MCp06SUB3_pt[ncentbins];
-TH1D * gN1MCp02SUB3_pt[ncentbins];
-
-TH1D * gN1MC22SUB3_pt[ncentbins];
-TH1D * gN1MC18SUB3_pt[ncentbins];
-TH1D * gN1MC14SUB3_pt[ncentbins];
-TH1D * gN1MC10SUB3_pt[ncentbins];
-TH1D * gN1MC06SUB3_pt[ncentbins];
-TH1D * gN1MC02SUB3_pt[ncentbins];
+TH1D * gN1MC22SUB3_pt[netabins][ncentbins];
+TH1D * gN1MC18SUB3_pt[netabins][ncentbins];
+TH1D * gN1MC14SUB3_pt[netabins][ncentbins];
 
 void plotPtDistEven()
 {
-    
+
     TH1::SetDefaultSumw2();
 
-    for (int cbin = 0; cbin<ncentbins; cbin++) {
-        gN1MCm22SUB2_eta[cbin] = new TH1D(Form("N1MCm22SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCm18SUB2_eta[cbin] = new TH1D(Form("N1MCm18SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCm14SUB2_eta[cbin] = new TH1D(Form("N1MCm14SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCm10SUB2_eta[cbin] = new TH1D(Form("N1MCm10SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCm06SUB2_eta[cbin] = new TH1D(Form("N1MCm06SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCm02SUB2_eta[cbin] = new TH1D(Form("N1MCm02SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
+    for (int ebin = 0; ebin<netabins; ebin++) {
+        for (int cbin = 0; cbin<ncentbins; cbin++) {
+            gN1MCm22SUB3_pt[ebin][cbin] = new TH1D(Form("N1MCm22SUB3_e%d_c%d_%d",ebin,cminCENT[cbin],cmaxCENT[cbin]), "", nptbins, ptbins);
+            gN1MCm18SUB3_pt[ebin][cbin] = new TH1D(Form("N1MCm18SUB3_e%d_c%d_%d",ebin,cminCENT[cbin],cmaxCENT[cbin]), "", nptbins, ptbins);
+            gN1MCm14SUB3_pt[ebin][cbin] = new TH1D(Form("N1MCm14SUB3_e%d_c%d_%d",ebin,cminCENT[cbin],cmaxCENT[cbin]), "", nptbins, ptbins);
 
-        gN1MCp22SUB2_eta[cbin] = new TH1D(Form("N1MCp22SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCp18SUB2_eta[cbin] = new TH1D(Form("N1MCp18SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCp14SUB2_eta[cbin] = new TH1D(Form("N1MCp14SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCp10SUB2_eta[cbin] = new TH1D(Form("N1MCp10SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCp06SUB2_eta[cbin] = new TH1D(Form("N1MCp06SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCp02SUB2_eta[cbin] = new TH1D(Form("N1MCp02SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
+            gN1MCp22SUB3_pt[ebin][cbin] = new TH1D(Form("N1MCp22SUB3_e%d_c%d_%d",ebin,cminCENT[cbin],cmaxCENT[cbin]), "", nptbins, ptbins);
+            gN1MCp18SUB3_pt[ebin][cbin] = new TH1D(Form("N1MCp18SUB3_e%d_c%d_%d",ebin,cminCENT[cbin],cmaxCENT[cbin]), "", nptbins, ptbins);
+            gN1MCp14SUB3_pt[ebin][cbin] = new TH1D(Form("N1MCp14SUB3_e%d_c%d_%d",ebin,cminCENT[cbin],cmaxCENT[cbin]), "", nptbins, ptbins);
 
-        gN1MC22SUB2_eta[cbin] = new TH1D(Form("N1MC22SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MC18SUB2_eta[cbin] = new TH1D(Form("N1MC18SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MC14SUB2_eta[cbin] = new TH1D(Form("N1MC14SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MC10SUB2_eta[cbin] = new TH1D(Form("N1MC10SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MC06SUB2_eta[cbin] = new TH1D(Form("N1MC06SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MC02SUB2_eta[cbin] = new TH1D(Form("N1MC02SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-
-        gN1MCm22SUB3_eta[cbin] = new TH1D(Form("N1MCm22SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCm18SUB3_eta[cbin] = new TH1D(Form("N1MCm18SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCm14SUB3_eta[cbin] = new TH1D(Form("N1MCm14SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCm10SUB3_eta[cbin] = new TH1D(Form("N1MCm10SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCm06SUB3_eta[cbin] = new TH1D(Form("N1MCm06SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCm02SUB3_eta[cbin] = new TH1D(Form("N1MCm02SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-
-        gN1MCp22SUB3_eta[cbin] = new TH1D(Form("N1MCp22SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCp18SUB3_eta[cbin] = new TH1D(Form("N1MCp18SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCp14SUB3_eta[cbin] = new TH1D(Form("N1MCp14SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCp10SUB3_eta[cbin] = new TH1D(Form("N1MCp10SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCp06SUB3_eta[cbin] = new TH1D(Form("N1MCp06SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MCp02SUB3_eta[cbin] = new TH1D(Form("N1MCp02SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-
-        gN1MC22SUB3_eta[cbin] = new TH1D(Form("N1MC22SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MC18SUB3_eta[cbin] = new TH1D(Form("N1MC18SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MC14SUB3_eta[cbin] = new TH1D(Form("N1MC14SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MC10SUB3_eta[cbin] = new TH1D(Form("N1MC10SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MC06SUB3_eta[cbin] = new TH1D(Form("N1MC06SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
-        gN1MC02SUB3_eta[cbin] = new TH1D(Form("N1MC02SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]), "", netabins, etabins);
+            gN1MC22SUB3_pt[ebin][cbin] = new TH1D(Form("N1MC22SUB3_e%d_c%d_%d",ebin,cminCENT[cbin],cmaxCENT[cbin]), "", nptbins, ptbins);
+            gN1MC18SUB3_pt[ebin][cbin] = new TH1D(Form("N1MC18SUB3_e%d_c%d_%d",ebin,cminCENT[cbin],cmaxCENT[cbin]), "", nptbins, ptbins);
+            gN1MC14SUB3_pt[ebin][cbin] = new TH1D(Form("N1MC14SUB3_e%d_c%d_%d",ebin,cminCENT[cbin],cmaxCENT[cbin]), "", nptbins, ptbins);
+        }
     }
+
+    for (int i = 0; i<NANALS; i++) {
+        for (int ebin = 0; ebin<netabins; ebin++) {
+            for (int cbin = 0; cbin<ncentbins; cbin++) {
+                TString tag = Form("figures_MH/%s/eta_%s_%s/data/%s_%d_%d_eta_%1.1f_%1.1f_A.dat",ANAL[i].Data(),etags[ebin].Data(),etags[ebin+1].Data(),ANAL[i].Data(),cminCENT[cbin],cmaxCENT[cbin],etabins[ebin],etabins[ebin+1]);
+                cout<<tag<<endl;
+            }
+        }
+    }
+    // for (int cbin = 0; cbin<ncentbins; cbin++) {
+    //     TString tag = Form("figures_MH/%s/EtaDistributions/data/EtaInt_%s_%d_%d.dat",ANAL[i].Data(),ANAL[i].Data(),cminCENT[cbin],cmaxCENT[cbin]);
+    //     // cout<<tag.Data()<<endl;
+    //     ifstream fin(tag.Data());
+    //
+    //     double eta, v1, v1e, v1A, v1Ae, v1B, v1Be;
+    //     int neta = 0;
+    //     while (fin >> eta >> v1 >> v1e >> v1A >> v1Ae >> v1B >> v1Be) {
+    //     // if (i == 0 && cbin == 0) cout<<"neta = "<<neta<<"\tdeltaEta = "<<deltaEta[neta]<<"\tv1A = "<<v1A<<"\tv1Ae = "<<v1Ae<<endl;
+    //
+    //     if (i == 0) {
+    //         gN1MCm22SUB2_eta[cbin]->SetBinContent(neta+1, v1A);
+    //         gN1MCm22SUB2_eta[cbin]->SetBinError(neta+1, v1Ae);
+    //     }
+    // }
 
 }
