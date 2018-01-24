75edab6b24da56c51b88513a5705b9cf08148c39
EnrichedWillium-237 <will.j.mcbrayer@gmail.com>
2018-01-23 20:31:28 -0600

    Update

--- macros/plotPtDist.C
+++ macros/plotPtDist.C
@@ -19,11 +19,12 @@ using namespace hi;
 static const int ncentbins = 13;
 static const int cminCENT[] = {0,  5, 10, 15, 20, 25, 30, 35, 40, 50, 60,  0, 20,  60};
 static const int cmaxCENT[] = {5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 20, 60, 100};
-static const int netabins = 14;
-static const double eminETA[] = {-2.4, -2.0, -1.6, -1.2, -0.8, -0.4,  0.0,  0.4,  0.8,  1.2,  1.6,  2.0, -2.4,  0.0};
-static const double emaxETA[] = {-2.0, -1.6, -1.2, -0.8, -0.4,  0.0,  0.4,  0.8,  1.2,  1.6,  2.0,  2.4,  0.0,  2.4};
+static const int netabins = 16;
+static const double eminETA[] = {-2.4, -2.0, -1.6, -1.2, -0.8, -0.4,  0.0,  0.4,  0.8,  1.2,  1.6,  2.0, -2.4,  0.0, -2.4,  0.4};
+static const double emaxETA[] = {-2.0, -1.6, -1.2, -0.8, -0.4,  0.0,  0.4,  0.8,  1.2,  1.6,  2.0,  2.4,  0.0,  2.4, -0.4,  2.4};
 static const double etaMid[] = {-2.2, -1.8, -1.4, -1.0, -0.6, -0.2,  0.2,  0.6,  1.0,  1.4,  1.8,  2.2};
-TString etags[] = {"-24_-20", "-20_-16", "-16_-12", "-12_-8", "-8_-4", "-4_0", "0_4", "4_8", "8_12", "12_16", "16_20", "20_24", "-24_0", "0_24"};
+TString etags[] = {"-24_-20", "-20_-16", "-16_-12", "-12_-8", "-8_-4", "-4_0", "0_4", "4_8", "8_12", "12_16", "16_20", "20_24",
+                   "-24_0", "0_24", "-24_-4", "4_24"};
 static const int nptbins = 18;
 static const double ptbins[] = {0.30,  0.40,  0.50,  0.60,  0.80,  1.00,  1.25,  1.50,  2.00,  2.50,  3.00,
                      3.50,  4.00,  5.00,  6.00,  7.00,  8.00,  10.00,  12.00};
@@ -113,6 +114,8 @@ void plotPtDist()
     }
 
     if (!fopen("results/Pt_Distributions","r")) system("mkdir results/Pt_Distributions");
+    if (!fopen("results/Pt_Distributions/N1MC22","r")) system("mkdir results/Pt_Distributions/N1MC22");
+    if (!fopen("results/Pt_Distributions/N1MC18","r")) system("mkdir results/Pt_Distributions/N1MC18");
 
     int erangeNeg = 0;
     int erangePos = 0;
@@ -126,66 +129,133 @@ void plotPtDist()
     erangeNeg = 13;
     erangePos = 12;
     crange = 1;
-    TCanvas * cN1MCpm22SUB2 = new TCanvas("cN1MCpm22SUB2","cN1MCpm22SUB2",650,600);
-    TPad * padN1MCpm22SUB2 = (TPad *) cN1MCpm22SUB2->cd();
-    padN1MCpm22SUB2->SetGrid();
-    TH1D * hN1MCpm22SUB2 = (TH1D *) h0->Clone("hN1MCpm22SUB2");
-    hN1MCpm22SUB2->SetYTitle("v_{1}^{even}");
-    hN1MCpm22SUB2->GetYaxis()->SetRangeUser(-0.03, 0.16);
-    hN1MCpm22SUB2->Draw();
-    N1MCm22SUB2[erangeNeg][crange]->SetMarkerColor(kRed);
-    N1MCm22SUB2[erangeNeg][crange]->SetLineColor(kRed);
-    N1MCm22SUB2[erangeNeg][crange]->SetMarkerStyle(24);
-    N1MCm22SUB2[erangeNeg][crange]->SetMarkerSize(1.2);
-    N1MCm22SUB2[erangeNeg][crange]->Draw("same");
-    N1MCp22SUB2[erangePos][crange]->SetMarkerColor(kBlue);
-    N1MCp22SUB2[erangePos][crange]->SetLineColor(kBlue);
-    N1MCp22SUB2[erangePos][crange]->SetMarkerStyle(24);
-    N1MCp22SUB2[erangePos][crange]->SetMarkerSize(1.2);
-    N1MCp22SUB2[erangePos][crange]->Draw("same");
-    TLegend * legN1MCpm22SUB2 = new TLegend(0.49, 0.17, 0.78, 0.27);
-    SetLegend(legN1MCpm22SUB2, 18);
-    legN1MCpm22SUB2->AddEntry(N1MCm22SUB2[erangeNeg][crange],"#Psi_{1}^{trk}{2SUB} (-2.4 < #eta < -2.0)","p");
-    legN1MCpm22SUB2->AddEntry(N1MCp22SUB2[erangePos][crange],"#Psi_{1}^{trk}{2SUB} (2.0 < #eta < 2.4)","p");
-    legN1MCpm22SUB2->Draw();
-    TPaveText * txN1MCpm22SUB2 = new TPaveText(0.19, 0.84, 0.44, 0.93, "NDC");
-    SetTPaveTxt(txN1MCpm22SUB2, 18);
-    txN1MCpm22SUB2->AddText(Form("POI: %1.1f < |#eta| < %1.1f",eminETA[erangeNeg],emaxETA[erangeNeg]));
-    txN1MCpm22SUB2->AddText(Form("(%d-%d%%)",cminCENT[crange],cmaxCENT[crange]));
-    txN1MCpm22SUB2->Draw();
-    cN1MCpm22SUB2->Print(Form("results/Pt_Distributions/N1MCpm22SUB2_%d_%d.png",cminCENT[crange],cmaxCENT[crange]),"png");
+    for (int cbin = 0; cbin<ncentbins; cbin++) {
+        TCanvas * cN1MCpm22SUB2 = new TCanvas(Form("cN1MCpm22SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]),"cN1MCpm22SUB2",650,600);
+        TPad * padN1MCpm22SUB2 = (TPad *) cN1MCpm22SUB2->cd();
+        padN1MCpm22SUB2->SetGrid();
+        TH1D * hN1MCpm22SUB2 = (TH1D *) h0->Clone(Form("hN1MCpm22SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]));
+        hN1MCpm22SUB2->SetYTitle("v_{1}^{even}");
+        hN1MCpm22SUB2->GetYaxis()->SetRangeUser(-0.03, 0.16);
+        hN1MCpm22SUB2->Draw();
+        N1MCm22SUB2[erangeNeg][cbin]->SetMarkerColor(kRed);
+        N1MCm22SUB2[erangeNeg][cbin]->SetLineColor(kRed);
+        N1MCm22SUB2[erangeNeg][cbin]->SetMarkerStyle(24);
+        N1MCm22SUB2[erangeNeg][cbin]->SetMarkerSize(1.2);
+        N1MCm22SUB2[erangeNeg][cbin]->Draw("same");
+        N1MCp22SUB2[erangePos][cbin]->SetMarkerColor(kBlue);
+        N1MCp22SUB2[erangePos][cbin]->SetLineColor(kBlue);
+        N1MCp22SUB2[erangePos][cbin]->SetMarkerStyle(24);
+        N1MCp22SUB2[erangePos][cbin]->SetMarkerSize(1.2);
+        N1MCp22SUB2[erangePos][cbin]->Draw("same");
+        TLegend * legN1MCpm22SUB2 = new TLegend(0.49, 0.17, 0.78, 0.27);
+        SetLegend(legN1MCpm22SUB2, 18);
+        legN1MCpm22SUB2->AddEntry(N1MCm22SUB2[erangeNeg][cbin],"#Psi_{1}^{trk}{2SUB} (-2.4 < #eta < -2.0)","p");
+        legN1MCpm22SUB2->AddEntry(N1MCp22SUB2[erangePos][cbin],"#Psi_{1}^{trk}{2SUB} (2.0 < #eta < 2.4)","p");
+        legN1MCpm22SUB2->Draw();
+        TPaveText * txN1MCpm22SUB2 = new TPaveText(0.19, 0.84, 0.44, 0.93, "NDC");
+        SetTPaveTxt(txN1MCpm22SUB2, 18);
+        txN1MCpm22SUB2->AddText(Form("POI: %1.1f < |#eta| < %1.1f",eminETA[erangeNeg],emaxETA[erangeNeg]));
+        txN1MCpm22SUB2->AddText(Form("(%d-%d%%)",cminCENT[cbin],cmaxCENT[cbin]));
+        txN1MCpm22SUB2->Draw();
+        cN1MCpm22SUB2->Print(Form("results/Pt_Distributions/N1MCpm22SUB2_%d_%d.png",cminCENT[cbin],cmaxCENT[cbin]),"png");
+        cN1MCpm22SUB2->Close();
 
 
-    TCanvas * cratN1MCpm22SUB2 = new TCanvas("cratN1MCpm22SUB2","cratN1MCpm22SUB2",650,600);
-    TPad * padratN1MCpm22SUB2 = (TPad *) cratN1MCpm22SUB2->cd();
-    padratN1MCpm22SUB2->SetGrid();
-    TH1D * hratN1MCpm22SUB2 = (TH1D *) h0->Clone("hratN1MCpm22SUB2");
-    hratN1MCpm22SUB2->SetYTitle("v_{1}^{even}{+#eta} / v_{1}^{even}{-#eta}");
-    hratN1MCpm22SUB2->GetYaxis()->SetRangeUser(0.6, 1.4);
-    hratN1MCpm22SUB2->Draw();
-    TH1D * ratN1MCpm22SUB2 = (TH1D *) N1MCp22SUB2[erangePos][crange]->Clone(Form("ratN1MCpm22SUB2_%d_%d",cminCENT[crange],cmaxCENT[crange]));
-    ratN1MCpm22SUB2->Divide(N1MCm22SUB2[erangeNeg][crange]);
-    for (int i = 1; i<=N1MCp22SUB2[erangeNeg][crange]->GetNbinsX(); i++) {
-        double x = N1MCp22SUB2[erangePos][crange]->GetBinContent(i);
-        double xe = N1MCp22SUB2[erangePos][crange]->GetBinError(i);
-        double y = N1MCm22SUB2[erangeNeg][crange]->GetBinContent(i);
-        double ye = N1MCm22SUB2[erangeNeg][crange]->GetBinError(i);
-        //ratN1MCpm22SUB2->SetBinError(i, ErrRatCalc(x, y, xe, ye));
+        TCanvas * cratN1MCpm22SUB2 = new TCanvas(Form("cratN1MCpm22SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]),"cratN1MCpm22SUB2",650,600);
+        TPad * padratN1MCpm22SUB2 = (TPad *) cratN1MCpm22SUB2->cd();
+        padratN1MCpm22SUB2->SetGrid();
+        TH1D * hratN1MCpm22SUB2 = (TH1D *) h0->Clone(Form("hratN1MCpm22SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]));
+        hratN1MCpm22SUB2->SetYTitle("v_{1}^{even}{+#eta} / v_{1}^{even}{-#eta}");
+        hratN1MCpm22SUB2->GetYaxis()->SetRangeUser(0.6, 1.4);
+        hratN1MCpm22SUB2->Draw();
+        TH1D * ratN1MCpm22SUB2 = (TH1D *) N1MCp22SUB2[erangePos][cbin]->Clone(Form("ratN1MCpm22SUB2_%d_%d",cminCENT[cbin],cmaxCENT[cbin]));
+        ratN1MCpm22SUB2->Divide(N1MCm22SUB2[erangeNeg][cbin]);
+        for (int i = 1; i<=N1MCp22SUB2[erangeNeg][cbin]->GetNbinsX(); i++) {
+            double x = N1MCp22SUB2[erangePos][cbin]->GetBinContent(i);
+            double xe = N1MCp22SUB2[erangePos][cbin]->GetBinError(i);
+            double y = N1MCm22SUB2[erangeNeg][cbin]->GetBinContent(i);
+            double ye = N1MCm22SUB2[erangeNeg][cbin]->GetBinError(i);
+            //ratN1MCpm22SUB2->SetBinError(i, ErrRatCalc(x, y, xe, ye));
+        }
+        ratN1MCpm22SUB2->SetMarkerColor(kBlack);
+        ratN1MCpm22SUB2->SetLineColor(kBlack);
+        ratN1MCpm22SUB2->SetMarkerStyle(20);
+        ratN1MCpm22SUB2->SetMarkerSize(1.1);
+        ratN1MCpm22SUB2->Draw("same");
+        TPaveText * txratN1MCpm22SUB2 = new TPaveText(0.19, 0.84, 0.44, 0.93, "NDC");
+        SetTPaveTxt(txratN1MCpm22SUB2, 18);
+        txratN1MCpm22SUB2->AddText(Form("POI: %1.1f < |#eta| < %1.1f",eminETA[erangeNeg],emaxETA[erangeNeg]));
+        txratN1MCpm22SUB2->AddText(Form("(%d-%d%%)",cminCENT[cbin],cmaxCENT[cbin]));
+        txratN1MCpm22SUB2->Draw();
+        TLine * lnratN1MCpm22SUB2 = new TLine(0.0, 1.0, 12.0, 1.0);
+        lnratN1MCpm22SUB2->SetLineWidth(2);
+        lnratN1MCpm22SUB2->Draw();
+        cratN1MCpm22SUB2->Print(Form("results/Pt_Distributions/N1MCpm22SUB2_ratio_%d_%d.png",cminCENT[cbin],cmaxCENT[cbin]),"png");
+        cratN1MCpm22SUB2->Close();
+
+
+        TCanvas * cN1MCpm22SUB3 = new TCanvas(Form("cN1MCpm22SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]),"cN1MCpm22SUB3",650,600);
+        TPad * padN1MCpm22SUB3 = (TPad *) cN1MCpm22SUB3->cd();
+        padN1MCpm22SUB3->SetGrid();
+        TH1D * hN1MCpm22SUB3 = (TH1D *) h0->Clone(Form("hN1MCpm22SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]));
+        hN1MCpm22SUB3->SetYTitle("v_{1}^{even}");
+        hN1MCpm22SUB3->GetYaxis()->SetRangeUser(-0.03, 0.16);
+        hN1MCpm22SUB3->Draw();
+        N1MCm22SUB3[erangeNeg][cbin]->SetMarkerColor(kRed);
+        N1MCm22SUB3[erangeNeg][cbin]->SetLineColor(kRed);
+        N1MCm22SUB3[erangeNeg][cbin]->SetMarkerStyle(24);
+        N1MCm22SUB3[erangeNeg][cbin]->SetMarkerSize(1.2);
+        N1MCm22SUB3[erangeNeg][cbin]->Draw("same");
+        N1MCp22SUB3[erangePos][cbin]->SetMarkerColor(kBlue);
+        N1MCp22SUB3[erangePos][cbin]->SetLineColor(kBlue);
+        N1MCp22SUB3[erangePos][cbin]->SetMarkerStyle(24);
+        N1MCp22SUB3[erangePos][cbin]->SetMarkerSize(1.2);
+        N1MCp22SUB3[erangePos][cbin]->Draw("same");
+        TLegend * legN1MCpm22SUB3 = new TLegend(0.49, 0.17, 0.78, 0.27);
+        SetLegend(legN1MCpm22SUB3, 18);
+        legN1MCpm22SUB3->AddEntry(N1MCm22SUB3[erangeNeg][cbin],"#Psi_{1}^{trk}{3SUB} (-2.4 < #eta < -2.0)","p");
+        legN1MCpm22SUB3->AddEntry(N1MCp22SUB3[erangePos][cbin],"#Psi_{1}^{trk}{3SUB} (2.0 < #eta < 2.4)","p");
+        legN1MCpm22SUB3->Draw();
+        TPaveText * txN1MCpm22SUB3 = new TPaveText(0.19, 0.84, 0.44, 0.93, "NDC");
+        SetTPaveTxt(txN1MCpm22SUB3, 18);
+        txN1MCpm22SUB3->AddText(Form("POI: %1.1f < |#eta| < %1.1f",eminETA[erangeNeg],emaxETA[erangeNeg]));
+        txN1MCpm22SUB3->AddText(Form("(%d-%d%%)",cminCENT[cbin],cmaxCENT[cbin]));
+        txN1MCpm22SUB3->Draw();
+        cN1MCpm22SUB3->Print(Form("results/Pt_Distributions/N1MCpm22SUB3_%d_%d.png",cminCENT[cbin],cmaxCENT[cbin]),"png");
+        cN1MCpm22SUB3->Close();
+
+
+        TCanvas * cratN1MCpm22SUB3 = new TCanvas(Form("cratN1MCpm22SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]),"cratN1MCpm22SUB3",650,600);
+        TPad * padratN1MCpm22SUB3 = (TPad *) cratN1MCpm22SUB3->cd();
+        padratN1MCpm22SUB3->SetGrid();
+        TH1D * hratN1MCpm22SUB3 = (TH1D *) h0->Clone(Form("hratN1MCpm22SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]));
+        hratN1MCpm22SUB3->SetYTitle("v_{1}^{even}{+#eta} / v_{1}^{even}{-#eta}");
+        hratN1MCpm22SUB3->GetYaxis()->SetRangeUser(0.6, 1.4);
+        hratN1MCpm22SUB3->Draw();
+        TH1D * ratN1MCpm22SUB3 = (TH1D *) N1MCp22SUB3[erangePos][cbin]->Clone(Form("ratN1MCpm22SUB3_%d_%d",cminCENT[cbin],cmaxCENT[cbin]));
+        ratN1MCpm22SUB3->Divide(N1MCm22SUB3[erangeNeg][cbin]);
+        for (int i = 1; i<=N1MCp22SUB3[erangeNeg][cbin]->GetNbinsX(); i++) {
+            double x = N1MCp22SUB3[erangePos][cbin]->GetBinContent(i);
+            double xe = N1MCp22SUB3[erangePos][cbin]->GetBinError(i);
+            double y = N1MCm22SUB3[erangeNeg][cbin]->GetBinContent(i);
+            double ye = N1MCm22SUB3[erangeNeg][cbin]->GetBinError(i);
+            //ratN1MCpm22SUB3->SetBinError(i, ErrRatCalc(x, y, xe, ye));
+        }
+        ratN1MCpm22SUB3->SetMarkerColor(kBlack);
+        ratN1MCpm22SUB3->SetLineColor(kBlack);
+        ratN1MCpm22SUB3->SetMarkerStyle(20);
+        ratN1MCpm22SUB3->SetMarkerSize(1.1);
+        ratN1MCpm22SUB3->Draw("same");
+        TPaveText * txratN1MCpm22SUB3 = new TPaveText(0.19, 0.84, 0.44, 0.93, "NDC");
+        SetTPaveTxt(txratN1MCpm22SUB3, 18);
+        txratN1MCpm22SUB3->AddText(Form("POI: %1.1f < |#eta| < %1.1f",eminETA[erangeNeg],emaxETA[erangeNeg]));
+        txratN1MCpm22SUB3->AddText(Form("(%d-%d%%)",cminCENT[cbin],cmaxCENT[cbin]));
+        txratN1MCpm22SUB3->Draw();
+        TLine * lnratN1MCpm22SUB3 = new TLine(0.0, 1.0, 12.0, 1.0);
+        lnratN1MCpm22SUB3->SetLineWidth(2);
+        lnratN1MCpm22SUB3->Draw();
+        cratN1MCpm22SUB3->Print(Form("results/Pt_Distributions/N1MCpm22SUB3_ratio_%d_%d.png",cminCENT[cbin],cmaxCENT[cbin]),"png");
+        cratN1MCpm22SUB3->Close();
     }
-    ratN1MCpm22SUB2->SetMarkerColor(kBlack);
-    ratN1MCpm22SUB2->SetLineColor(kBlack);
-    ratN1MCpm22SUB2->SetMarkerStyle(20);
-    ratN1MCpm22SUB2->SetMarkerSize(1.1);
-    ratN1MCpm22SUB2->Draw("same");
-    TPaveText * txratN1MCpm22SUB2 = new TPaveText(0.19, 0.84, 0.44, 0.93, "NDC");
-    SetTPaveTxt(txratN1MCpm22SUB2, 18);
-    txratN1MCpm22SUB2->AddText(Form("POI: %1.1f < |#eta| < %1.1f",eminETA[erangeNeg],emaxETA[erangeNeg]));
-    txratN1MCpm22SUB2->AddText(Form("(%d-%d%%)",cminCENT[crange],cmaxCENT[crange]));
-    txratN1MCpm22SUB2->Draw();
-    TLine * lnratN1MCpm22SUB2 = new TLine(0.0, 1.0, 12.0, 1.0);
-    lnratN1MCpm22SUB2->SetLineWidth(2);
-    lnratN1MCpm22SUB2->Draw();
-    cratN1MCpm22SUB2->Print(Form("results/Pt_Distributions/N1MCpm22SUB2_ratio_%d_%d.png",cminCENT[crange],cmaxCENT[crange]),"png");
-
 
 }
